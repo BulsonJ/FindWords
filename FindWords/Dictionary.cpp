6645e46e5d2af30f181de001c24d05848c5cd756
Jack Bulson <jacktbulson@gmail.com>
2022-02-24 16:47:57 +0000

    Added Template Trie class

--- FindWords/Dictionary.cpp
+++ FindWords/Dictionary.cpp
@@ -11,14 +11,14 @@ Dictionary::Dictionary(const std::string& filename) throw (std::invalid_argument
 		throw std::invalid_argument("no dictionary file available: " + filename);
 	}
 
-	root = std::make_unique<TrieNode>();
+	root = std::make_unique<Trie<s_AlphabetSize, s_AlphabetStart>>();
 
 	std::string text;
 	while (std::getline(english_dictionary, text)) {
 		if (CheckValidWord(text)) {
 			throw std::invalid_argument("non alpha characters in dictionary file");
 		}
-		TrieInsert(text);
+		root->Insert(text);
 	}
 
 	if (root == nullptr) {
@@ -27,11 +27,11 @@ Dictionary::Dictionary(const std::string& filename) throw (std::invalid_argument
 }
 
 bool Dictionary::CheckWordsCouldExist(const std::string& word) const {
-	return TriePrefix(word);
+	return root->Prefix(word);
 }
 
 bool Dictionary::CheckWordExists(const std::string& word) const {
-	return TrieLookup(word);
+	return root->Lookup(word);
 }
 
 bool Dictionary::CheckValidWord(const std::string& word) {
@@ -39,56 +39,10 @@ bool Dictionary::CheckValidWord(const std::string& word) {
 	return word.find_first_not_of("abcdefghijklmnopqrstuvwxyz") != std::string::npos;
 }
 
-/*
-Reference for Trie structure:
-https://www.techiedelight.com/cpp-implementation-trie-data-structure/
-
-Trie structure was used to allow the program to quickly determine if a word can possibly exist.
-Could be moved to a more generic Trie class/struct in the future.
-*/
 
 
-void Dictionary::TrieInsert(const std::string& word) {
-	TrieNode* current = root.get();
-	for (int i = 0; i < word.length(); i++) {
-		if (current->children[word[i] - s_AlphabetStart].get() == nullptr) {
-			current->children[word[i] - s_AlphabetStart] = std::make_unique<TrieNode>();
-		}
-		current = current->children[word[i] - s_AlphabetStart].get();
-	}
-	current->end_of_word = true;
-}
 
-bool Dictionary::TrieLookup(const std::string& word) const {
-	TrieNode* current = root.get();
-	for (int i = 0; i < word.length(); i++)
-	{
-		if (current->children[word[i] - s_AlphabetStart].get() == nullptr) {
-			return false;
-		}
-		current = current->children[word[i] - s_AlphabetStart].get();
-	}
-	return current->end_of_word;
-}
 
-bool Dictionary::TriePrefix(const std::string& word) const {
-	TrieNode* current = root.get();
-
-	for (int i = 0; i < word.length(); i++)
-	{
-		if (current->children[word[i] - s_AlphabetStart] == nullptr) {
-			return false;
-		}
-		current = current->children[word[i] - s_AlphabetStart].get();
-	}
-
-	for (int i = 0; i < s_AlphabetSize; i++) {
-		if (current->children[i] != nullptr) {
-			return true;
-		}
-	}
-	return true;
-}
 
 
 
--- FindWords/Dictionary.h
+++ FindWords/Dictionary.h
@@ -2,6 +2,7 @@
 #include <set>
 #include <unordered_set>
 #include <memory>
+#include "Trie.h"
 
 #pragma once
 
@@ -17,18 +18,11 @@ protected:
 	static const int s_AlphabetSize = 26;
 	static const int s_AlphabetStart = 97;
 
-	struct TrieNode {
-		std::unique_ptr<TrieNode> children[Dictionary::s_AlphabetSize];
-		bool end_of_word = false;
-	};
-
-	std::unique_ptr<TrieNode> root;
+	std::unique_ptr<Trie<s_AlphabetSize, s_AlphabetStart>>root;
 
 	static bool CheckValidWord(const std::string& word);
 
-	void TrieInsert(const std::string& word);
-	bool TrieLookup(const std::string& word) const;
-	bool TriePrefix(const std::string& word) const;
+
 };
 
 
